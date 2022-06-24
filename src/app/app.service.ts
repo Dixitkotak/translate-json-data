38cb34a0714bdc322c8f669175c85872b2be3753
Dixitkotak <dixit.kotak@gmail.com>
2022-06-24 14:26:53 +0530

    1 file modified

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -11,11 +11,6 @@ export class AppService {
   constructor(private http: HttpClient) {}
 
   getTranslation(id: string, lg: string): Observable<any> {
-    //Write code calling service to obtain food fact data
-    //https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=${id} spenish
-    //https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=et&dt=t&q=${id} Estonian
-    //https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=hu&dt=t&q=${id} hugarian
-    //https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=ru&dt=t&q=${id} Russ.
     let url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${lg}&dt=t&q=${id}`;
     return this.http.get(url);
   }
